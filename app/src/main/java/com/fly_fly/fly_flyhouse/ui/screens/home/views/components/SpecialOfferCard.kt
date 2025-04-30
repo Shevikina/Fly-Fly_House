d0355603cf8c9f1bfe23a61e56e965728934b316
Shevikina <Shevik.vlog@gmail.com>
2025-04-30 16:06:06 +0300

    Upd gradle

--- app/src/main/java/com/fly_fly/fly_flyhouse/ui/screens/home/views/components/SpecialOfferCard.kt
+++ app/src/main/java/com/fly_fly/fly_flyhouse/ui/screens/home/views/components/SpecialOfferCard.kt
@@ -19,7 +19,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
+import coil3.compose.AsyncImage
 import com.fly_fly.fly_flyhouse.ui.theme.FlyFlyHouseTheme
 import com.fly_fly.fly_flyhouse.ui.theme.JetFlyFlyHouseTheme
 import com.fly_fly.fly_flyhouse.utils.DottedDivider
